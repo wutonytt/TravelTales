beed34cbc373bad8e5f0d5bb531d3b13d2b8fbf9
TZU-JUI Chen <179-chentzj@users.noreply.git.cs.nctu.edu.tw>
2023-11-08 00:49:36 -0600

    complete_selecting_multi_photo_not_able_to_use_with_nav

--- app/src/main/java/edu/illinois/cs465/traveltales/ui/add/AddFragment.java
+++ app/src/main/java/edu/illinois/cs465/traveltales/ui/add/AddFragment.java
@@ -1,5 +1,6 @@
 package edu.illinois.cs465.traveltales.ui.add;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,17 +9,41 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+import edu.illinois.cs465.traveltales.MainActivity;
+import edu.illinois.cs465.traveltales.R;
+import edu.illinois.cs465.traveltales.RecyclerAdapter;
+import edu.illinois.cs465.traveltales.databinding.ActivityMainBinding;
 import edu.illinois.cs465.traveltales.databinding.FragmentAddBinding;
 
 public class AddFragment extends Fragment {
 
     private FragmentAddBinding binding;
 
+    RecyclerView recyclerView;
+    TextView textView;
+    Button choose_picture;
+    ArrayList<Uri> uri = new ArrayList<>();
+    RecyclerAdapter adapter;
+
+    private static final int Read_Permission = 101;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
         AddViewModel addViewModel =
                 new ViewModelProvider(this).get(AddViewModel.class);
 
