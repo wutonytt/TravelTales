ad2620bf63cbdcbf874cc8739c3787035feb4170
Riya Shah <rshah257@illinois.edu>
2023-11-15 20:13:46 -0600

    map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,4 +46,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//    maps api dependencies
+    implementation("com.google.android.gms:play-services-maps:17.0.1")
+    implementation("com.google.android.gms:play-services-location:18.0.0")
 }
--- app/src/main/java/edu/illinois/cs465/traveltales/ui/map/MapFragment.java
+++ app/src/main/java/edu/illinois/cs465/traveltales/ui/map/MapFragment.java
@@ -10,11 +10,24 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+//import edu.illinois.cs465.traveltales.ui.location.LocationActivity;
+import edu.illinois.cs465.traveltales.R;
+
 import edu.illinois.cs465.traveltales.databinding.FragmentMapBinding;
 
-public class MapFragment extends Fragment {
+
+
+public class MapFragment extends Fragment implements OnMapReadyCallback, GoogleMap.OnMapClickListener{
 
     private FragmentMapBinding binding;
+    private GoogleMap googleMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -23,6 +36,18 @@ public class MapFragment extends Fragment {
 
         binding = FragmentMapBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            String apiKey = getString(R.string.google_maps_key);
+            mapFragment.getMapAsync(googleMap1 -> {
+                if (googleMap != null) {
+                    googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+//                    googleMap.setMyLocationEnabled(true);
+                }
+            });
+        }
 
         final TextView textView = binding.textMap;
         mapViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
@@ -34,4 +59,45 @@ public class MapFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        googleMap = map;
+        if (googleMap != null) {
+            String apiKey = getString(R.string.google_maps_key);
+
+            // Set up markers for different locations
+            LatLng location1 = new LatLng(37.7749, -122.4194);
+            LatLng location2 = new LatLng(37.7749, -122.4294);
+
+            googleMap.addMarker(new MarkerOptions().position(location1).title("Location 1"));
+            googleMap.addMarker(new MarkerOptions().position(location2).title("Location 2"));
+
+//        googleMap.setOnMarkerClickListener(this);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location1, 12));
+        }
+
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        return;
+    }
+//
+//    @Override
+//    public boolean onMarkerClick(Marker marker) {
+//        // Handle marker click, navigate to location page
+//        navigateToLocationPage(marker.getTitle());
+//        return true;
+//    }
+
+//    private void navigateToLocationPage(String locationTitle) {
+//        // Start a new activity or fragment for the specific location
+//        // Pass any necessary data to the location page
+//        // For example, using Intent to start a new activity:
+//        Intent locationIntent = new Intent(getActivity(), LocationActivity.class);
+//        locationIntent.putExtra("locationTitle", locationTitle);
+//        startActivity(locationIntent);
+//    }
+
 }
