0fad8b456af54668a85977915be24fc30a9abba9
Shreyachaudhary1112 <shreyuchaudhary@gmail.com>
2023-11-16 23:15:29 -0600

    revert gradle version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
-buildscript {
-    val agp_version by extra("8.0.0")
-    val agp_version1 by extra("8.0.1")
-}
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.0.1" apply false
+    id("com.android.application") version "8.1.3" apply false
 }
