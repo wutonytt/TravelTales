3ba9a798b88a75c07c26e4802a81ac31f1f5fc14
Shreyachaudhary1112 <shreyuchaudhary@gmail.com>
2023-11-30 21:06:42 -0600

    See description button works and changed the search suggestions

--- app/src/main/java/edu/illinois/cs465/traveltales/ui/search/model/ArrayListModel.java
+++ app/src/main/java/edu/illinois/cs465/traveltales/ui/search/model/ArrayListModel.java
@@ -10,14 +10,14 @@ public class ArrayListModel {
 
         ArrayList<PlacesModel> arrayList = new ArrayList<>();
         arrayList.add(new PlacesModel(R.drawable.amsterdam1, "Amsterdam"));
-        arrayList.add(new PlacesModel(R.drawable.b, "Amazon"));
+        arrayList.add(new PlacesModel(R.drawable.b, "Almere"));
         arrayList.add(new PlacesModel(R.drawable.c, "Europe"));
-        arrayList.add(new PlacesModel(R.drawable.p, "Australia"));
-        arrayList.add(new PlacesModel(R.drawable.l, "Japan"));
-        arrayList.add(new PlacesModel(R.drawable.m, "India"));
-        arrayList.add(new PlacesModel(R.drawable.n, "Sahara"));
-        arrayList.add(new PlacesModel(R.drawable.h, "Australia"));
-        arrayList.add(new PlacesModel(R.drawable.o, "Dubai"));
+        arrayList.add(new PlacesModel(R.drawable.p, "Hrleme"));
+        arrayList.add(new PlacesModel(R.drawable.l, "Belgium"));
+        arrayList.add(new PlacesModel(R.drawable.m, "Brussels"));
+        arrayList.add(new PlacesModel(R.drawable.n, "Germany"));
+        arrayList.add(new PlacesModel(R.drawable.h, "Netherland"));
+        arrayList.add(new PlacesModel(R.drawable.o, "A"));
         arrayList.add(new PlacesModel(R.drawable.t, "Paris"));
         arrayList.add(new PlacesModel(R.drawable.u, "Egypt"));
         arrayList.add(new PlacesModel(R.drawable.q, "Goa"));
